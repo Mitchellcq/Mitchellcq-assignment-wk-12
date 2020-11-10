2661cb17dc63d9f9fa86457cdc9557e7251f2122
Chee Quee <mitchellcq@gmail.com>
2020-11-10 18:20:14 +1100

    added jaws to connection

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('BBQ Brisket double Bacon Jim Beam Whisky Burger');
+INSERT INTO burgers (name) VALUES ('Rare Beef Top Sirloin seared with the Gunpowder from a sawn-off Shotgun Burger');
+INSERT INTO burgers (name) VALUES ('Collapsible Stock M-16 Mozzarella Stick Freedom Burger');
+INSERT INTO burgers (name) VALUES ('Jerk Prawn and Lime Buckshot Blast Burger');
+INSERT INTO burgers (name) VALUES ('Murphys Irish Stout Flame-broiled Strava burger with Kudos');
+INSERT INTO burgers (name) VALUES ('Jose Cuervo Nacho Cheese Queso Burger');
